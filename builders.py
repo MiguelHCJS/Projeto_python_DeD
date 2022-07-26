c1acf1038b064503ea87bd1315c983b7bf3982a8
Tenorio <miguel.h.c.j.s@gmail.com>
2022-07-25 22:25:18 -0300

    Atualização

---
+++ builders.py
@@ -0,0 +1,19 @@
+
+class Monstro:
+
+    def __init__(self, nome, forca, destreza, constituicao, inteligencia, sabedoria, carisma, deslocamento, armadura):
+        self.nome = nome
+        self.forca = forca
+        self.destreza = destreza
+        self.constituicao = constituicao
+        self.inteligencia = inteligencia
+        self.sabedoria = sabedoria
+        self.carisma = carisma
+        self.deslocamento = deslocamento
+        self.armadura = armadura
+
+class tamanhoMonstro:
+
+    def __init__(self, nomeTamanho, vida):
+        self.nomeTamanho = nomeTamanho
+        self.vida = vida
