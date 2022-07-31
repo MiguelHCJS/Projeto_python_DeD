4b8be70f996dfd119fc510add9df5468fd0f59dc
Miguel Tenório <miguel.h.c.j.s@gmail.com>
2022-07-31 19:36:00 -0300

    Otimizando o builders e o readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Projeto_python_D&D
- Desenvolvendo python e SQL, com D&D
+# Projeto D&D com python
+
+ Desenvolvendo uma aplicação que receba e armazene dados para a criação de monstros, a principio. Usando como base um jogo de RPG, D&D.
+
+ Com o objetivo de desenvolver meu conhecimento e experiencia em python, junto a algo que eu gosto que é jogar RPG...
+
+ Como objetivo para este projeto, almejo que ele facilite a criação de contexto e conteúdo para mestres de RPG com uma praticidade que os permitam forcar na história e não em meros detalhes que possam ser facilmente criados.
--- builders.py
+++ builders.py
@@ -1,26 +1,26 @@
 class Monstro:
 
     def __init__(self, nome, forca, destreza, constituicao, inteligencia, sabedoria, carisma, deslocamento, armadura):
-        self.nome = nome
-        self.forca = forca
-        self.destreza = destreza
-        self.constituicao = constituicao
-        self.inteligencia = inteligencia
-        self.sabedoria = sabedoria
-        self.carisma = carisma
-        self.deslocamento = deslocamento
-        self.armadura = armadura
+        self.__nome = nome
+        self.__forca = forca
+        self.__destreza = destreza
+        self.__constituicao = constituicao
+        self.__inteligencia = inteligencia
+        self.__sabedoria = sabedoria
+        self.__carisma = carisma
+        self.__deslocamento = deslocamento
+        self.__armadura = armadura
 
     def atacar_fisico(self, ataque, elemento, forca):
         self.ataque = ataque
         self.elemento = elemento
         self.carisma = forca
-        
+
     def atacar_distancia(self, ataque, elemento, destreza):
         self.ataque = ataque
         self.elemento = elemento
         self.carisma = destreza
-    
+
     def atacar_magico(self, ataque, elemento, sabedoria):
         self.ataque = ataque
         self.elemento = elemento
@@ -30,8 +30,9 @@ class Monstro:
         self.destreza = destreza
         self.armadura = armadura
 
+
 class tamanhoMonstro:
 
     def __init__(self, nomeTamanho, vida):
-        self.nomeTamanho = nomeTamanho
-        self.vida = vida
+        self.__nomeTamanho = nomeTamanho
+        self.__vida = vida
