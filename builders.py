e315a3877f17f4342c3d2d5a1b8626fec95237c6
Miguel Tenório <miguel.h.c.j.s@gmail.com>
2022-08-03 22:21:55 -0300

    monstros v1 - construção de monstros

--- builders.py
+++ builders.py
@@ -1,6 +1,6 @@
 class Monstro:
 
-    def __init__(self, nome, vida, forca, destreza, constituicao, inteligencia, sabedoria, carisma, deslocamento, armadura):
+    def __init__(self, nome, vida, forca, destreza, constituicao, inteligencia, sabedoria, carisma, deslocamento, armadura, tipo_monstro):
         self.__nome = nome
         self.__forca = forca
         self.__destreza = destreza
@@ -11,11 +11,37 @@ class Monstro:
         self.__deslocamento = deslocamento
         self.__armadura = armadura
         self.__vida = vida
+        self.__tipo_monstro = tipo_monstro
 
-    def status(self):
-        print(f'O monstro está com a vida em {self.__vida}')
+    @property  # Acesso direto com o "."
+    def hp(self):
+        print(f'O {self.__nome} está com a vida em {self.__vida}')
 
-    def defenda(self, destreza, armadura):
+    @hp.setter  # Parametro é passado atribuindo-o "="
+    def hp(self, valor):
+        self.__vida = valor
+
+    @staticmethod
+    def tipo_monstro():
+        tipos = {
+            "1": "Aberração",
+            "2": "Besta",
+            "3": "Celestial",
+            "4": "Constructo",
+            "5": "Corruptor",
+            "6": "Dragão",
+            "7": "Elemental",
+            "8": "Fada",
+            "9": "Gigante",
+            "10": "Humanoide",
+            "11": "Limo",
+            "12": "Monstruosidade",
+            "13": "Morto-vivo",
+            "14": "Planta"
+        }
+        return tipos
+
+    def defende(self, destreza, armadura):
         self.__destreza = destreza
         self.__armadura = armadura
 
